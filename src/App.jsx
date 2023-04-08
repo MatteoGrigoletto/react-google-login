8b109d5d231dd39c1f202acbf438f33742d5be94
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-04-08 16:20:02 +0200

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import "./App.scss";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
 import Login from "./pages/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/post/:id",
+      element: <Post />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
   return (
     <div className="App">
       <Navbar />
-      <Login />
+      <RouterProvider router={router} />
     </div>
   );
 }
