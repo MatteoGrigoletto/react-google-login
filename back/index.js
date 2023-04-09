1388c933115542ecf0596a0524007f3012b4c307
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-04-09 10:07:16 +0200

    add express and create cookieSession

---
+++ back/index.js
@@ -0,0 +1,27 @@
+const cookieSession = require("cookie-session");
+const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["lama"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credential: true,
+  })
+);
+
+app.listen("5000", () => {
+  console.log("server on");
+});
