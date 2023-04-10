11e812a29ca00873b4b04ec6726f4064a07691b2
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-04-11 01:24:05 +0200

    fix post height

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -3,7 +3,6 @@ import "./Post.scss";
 import { posts } from "../data";
 import { useLocation } from "react-router-dom";
 
-import Video from "../components/Video";
 function Post() {
   const location = useLocation().pathname.split("/").pop();
 
@@ -12,7 +11,6 @@ function Post() {
   return (
     <div className="post">
       <img src={post.img} alt="" className="post_img" />
-      <Video video={post.video} image={post.img} />
       <h1 className="post_title">{post.title}</h1>
       <p className="post_desc">{post.desc}</p>
       <p className="post_longdesc">{post.longDesc}</p>
